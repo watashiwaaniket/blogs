8c911aa044bdbf3922c5f9b09ed54787207d2ca5
watashiwaaniket <aniketdhakane3@gmail.com>
2026-07-14 18:58:17 +0530

    updated the favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "blogs by aniket",
+  title: "Aniket's Blogs",
   description: "A repository of my journey and learnings",
 };
 
